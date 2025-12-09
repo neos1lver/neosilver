567ae2ec277110e57ba2d40eb614925fd2e9aa61
neos1lver <juanocju@gmail.com>
2025-12-08 19:46:28 -0600

    Primer commit del proyecto

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Stack, Text, Link, FontWeights, IStackTokens, IStackStyles, ITextStyles } from '@fluentui/react';
+import logo from './logo.svg';
+import './App.css';
+
+const boldStyle: Partial<ITextStyles> = { root: { fontWeight: FontWeights.semibold } };
+const stackTokens: IStackTokens = { childrenGap: 15 };
+const stackStyles: Partial<IStackStyles> = {
+  root: {
+    width: '960px',
+    margin: '0 auto',
+    textAlign: 'center',
+    color: '#605e5c',
+  },
+};
+
+export const App: React.FunctionComponent = () => {
+  return (
+    <Stack horizontalAlign="center" verticalAlign="center" verticalFill styles={stackStyles} tokens={stackTokens}>
+      <Text variant="xxLarge" styles={boldStyle}>
+        neosilver 
+      </Text>
+      <Text variant="large">En construcción</Text>
+    </Stack>
+  );
+};
